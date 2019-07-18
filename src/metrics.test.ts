5d2914c0255cb2d38505904ee30787c0370d3cca
Tiago FRANCO FINOTTI <tiago.francofinotti@edu.ece.fr>
2019-07-18 10:48:12 +0200

    Making Travis work

--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,9 +1,9 @@
 import {expect} from 'chai'
 
-const a: number = 0
+const a: number = 1
 
 describe('Metrics', function () {
   it('should save and get', function () {
-    expect(a).to.equal(0)
+    expect(a).to.equal(1)
   })
 })
