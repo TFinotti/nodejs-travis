32cde123603546958848c9f01006e92e25715263
Tiago FRANCO FINOTTI <tiago.francofinotti@edu.ece.fr>
2019-07-19 11:19:05 +0200

    LAST COMMIT FFS

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
 import express = require('express')
 var bodyparser = require('body-parser')
+var cookieParser = require('cookie-parser')
 
 const app = express()
 
 import path = require('path')
 app.use(express.static(path.join(__dirname, '/../public')))
 
+app.use(cookieParser())
+
 app.set('views', __dirname + "/../views")
 app.set('view engine', 'ejs');
-
-app.get('/', (req: any, res: any) => {
-  res.render('main.ejs');
-})
 
 app.get('/hello/:name', (req: any, res: any) => {
   if (req.params.name === "Tiago") {
@@ -21,7 +20,6 @@ app.get('/hello/:name', (req: any, res: any) => {
   }
 })
 
-import { MetricsHandler, Metric } from './metrics'
 // app.get('/metrics.json', (req: any, res: any) => {
 //   MetricsHandler.get((err: Error | null, result?: any) => {
 //     if (err) {
@@ -32,12 +30,16 @@ import { MetricsHandler, Metric } from './metrics'
 // })
 
 // Initialize connection once
+import { MetricsHandler, Metric } from './metrics'
 var db: any
+var dbUser: any
+import { UserHandler, User } from './user'
 var mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient // Create a new MongoClient
 MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, (err: any, client: any) => {
   if (err) throw err
   db = client.db('mydb')
+  dbUser = new UserHandler(db)
 
   // Start the application after the database connection is ready
   const port: string = process.env.PORT || '8115'
@@ -54,7 +56,6 @@ app.use(bodyparser.urlencoded({ extended: true }))
 
 app.post('/metrics', (req: any, res: any) => {
   if (req.body) {
-    console.log(req.body)
     const metric = new Metric('Random', parseInt(req.body.value));
     new MetricsHandler(db).save(metric, (err: any, result: any) => {
       if (err)
@@ -80,4 +81,88 @@ app.delete('/metrics', (req: any, res: any) => {
       return res.status(500).json({ error: err, result: result });
     res.status(201).json({ error: err, result: result })
   })
+})
+
+import session = require('express-session')
+import ConnectMongo = require('connect-mongo')
+const MongoStore = ConnectMongo(session)
+
+app.use(session({
+  secret: 'user session',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore({ url: 'mongodb://localhost/mydb' })
+}))
+
+const authRouter = express.Router()
+
+authRouter.get('/login', function (req: any, res: any) {
+  
+  console.log(req)
+  res.render('login')
+})
+
+authRouter.get('/signup', function (req: any, res: any) {
+  res.render('signup')
+})
+
+
+authRouter.get('/logout', function (req: any, res: any) {
+  if (req.session.loggedIn) {
+    delete req.session.loggedIn
+    delete req.session.user
+  }
+  res.redirect('/login')
+})
+
+authRouter.post('/login', function (req: any, res: any, next: any) {
+  dbUser.get(req.body.username, function (err: Error | null, result: User | null) {
+    if (err) next(err)
+    if (result === null || !result.validatePassword(req.body.password)) {
+      console.log('login')
+      res.redirect('/login')
+    } else {
+      req.session.loggedIn = true
+      req.session.user = result
+      res.redirect('/')
+    }
+  })
+})
+
+app.use(authRouter)
+
+const userRouter = express.Router()
+
+userRouter.post('/signup', function (req: any, res: any, next: any) {
+  dbUser.get(req.body.username, function (err: Error | null, result: User | null) {
+    if (err) next(err)
+    if (result) {
+      res.status(409).send("user already exists")
+    } else {
+      dbUser.save(req.body, function (err: Error | null) {
+        if (err) next(err)
+        else res.status(201).send("user persisted")
+      })
+    }
+  })
+})
+
+userRouter.get('/:username', (req: any, res: any, next: any) => {
+  dbUser.get(req.params.username, function (err: Error | null, result: User | null) {
+    if (err || result === undefined) {
+      res.status(404).send("user not found")
+    } else res.status(200).json(result)
+  })
+})
+
+app.use(userRouter)
+
+const authCheck = function (req: any, res: any, next: any) {
+  if (req.session.loggedIn) {
+    next()
+  } else res.redirect('/login')
+}
+
+app.get('/', authCheck, (req: any, res: any) => {
+  res.render('main.ejs');
 })
--- src/metrics.test.ts
+++ src/metrics.test.ts
@@ -1,9 +1,70 @@
-import {expect} from 'chai'
+import { expect } from 'chai'
 
-const a: number = 1
+import { Metric, MetricsHandler } from './metrics'
+import mongodb from 'mongodb'
 
-describe('Metrics', function () {
-  it('should save and get', function () {
-    expect(a).to.equal(1)
+var dbMet: MetricsHandler
+var db: any
+var clientDb: any
+
+var mongoAsync = (callback: any) => {
+  const MongoClient = mongodb.MongoClient // Create a new MongoClient
+  MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, (err: any, client: any) => {
+    if (err) throw err
+    callback(client)
+  });
+}
+
+describe('Metrics', () => {
+  before((done) => {
+    mongoAsync((client: any) => {
+      clientDb = client
+      db = clientDb.db('mydb')
+      dbMet = new MetricsHandler(db)
+      done()
+    })
+  })
+
+  after(() => {
+    clientDb.close()
+  })
+
+  describe('/get', () => {
+    it('should get empty array', function () {
+      dbMet.get(1, function (err: Error | null, result?: Metric[]) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.be.empty
+      })
+    })
+  })
+
+  describe('/save', () => {
+    it('should save data', function () {
+      const metric = new Metric('Random', 5);
+      dbMet.save(metric, function (err: Error | null, result?: any) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.not.be.empty
+      })
+    })
+  })
+
+  describe('/delete', () => {
+    it('should delete data', function () {
+      dbMet.delete(5, function (err: Error | null, result?: any) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.not.be.empty
+      })
+    })
+
+    it('should not fail if data does not exist', function () {
+      dbMet.delete(3, function (err: Error | null, result?: any) {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.not.be.empty
+      })
+    })
   })
 })
